f494fd35f5281297645a050e19353bc9f3a8c827
jackyphung <jackyphung.393@gmail.com>
2022-06-10 13:31:07 -0400

    added project

---
+++ jaesboutique/AddItemController.swift
@@ -0,0 +1,30 @@
+//
+//  AddItemController.swift
+//  jaesboutique
+//
+//  Created by Jacky Phung on 2019-03-26.
+//  Copyright © 2019 Jacky Phung. All rights reserved.
+//
+
+import UIKit
+
+class AddItemController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ jaesboutique/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  jaesboutique
+//
+//  Created by Jacky Phung on 2019-03-20.
+//  Copyright © 2019 Jacky Phung. All rights reserved.
+//
+
+import Foundation
